6b19baab45e2d935f907e85c57b64e4bd49a7a9e
nam <70477669+ducnam0902@users.noreply.github.com>
2024-10-11 10:18:02 +0700

    Feature | Add a job to send an email about incomplete note tasks. (#23)
    * Bugfix | Add image for email
    
    * Job | Send an email at 9 PM to notify about incompleted note
    
    ---------
    
    Co-authored-by: Le Duc Nam <arthit@Joong.local>

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,9 +1,9 @@
 import { MailerService } from '@nestjs-modules/mailer';
 import { Injectable } from '@nestjs/common';
+import  * as moment from 'moment';
+import { NoteEntity } from 'src/note/note.entity';
 import { UserEntity } from 'src/user/user.entity';
 import envConfig from 'src/utils/config';
-
-
 @Injectable()
 export class MailService {
     constructor(private mailerService: MailerService) {}
@@ -15,9 +15,32 @@ export class MailService {
           template: './confirmation',
           context: {
             name: user.firstName + ' ' + user.lastName,
-            image: envConfig.PUBLIC_API_ENDPOINT + '/user/image/logo.png',
+            image: envConfig.PUBLIC_API_ENDPOINT + '/logo.png',
           },
         });
       }
+
+    async sendEmailNoteIsNotCompleted(task: NoteEntity[], user: UserEntity) {
+      const isFinishedAllTasks = task.every(item => item.isCompleted );
+      const incompletedTask = task.filter(item => !item.isCompleted);
+      const completedTask = task.filter(item => item.isCompleted);
+      const today = moment().format('DD-MMM-YYYY');
+      const subject = isFinishedAllTasks ? `Congratulation! You have completed all tasks on ${today}` :
+        `Reminder: You have remaining tasks that need to be done by the end of ${today}`;
+      await this.mailerService.sendMail({
+        from: 'dailymanagementteam@gmail.com',
+        to: user.email,
+        subject: subject + ' | Daily Management Application',
+        template: './dailyNoteNotCompleted',
+        context: {
+          name: user.firstName + ' ' + user.lastName,
+          image: envConfig.PUBLIC_API_ENDPOINT + '/logo.png',
+          incompletedTask: incompletedTask,
+          haveTaskCompleted: completedTask.length !== 0,
+          completedTask: completedTask,
+          isFinishedAllTask: isFinishedAllTasks,
+        },
+      })
+    }
 
 }
