da689a53f389429aad57fcae048a6384ec2563eb
nam <70477669+ducnam0902@users.noreply.github.com>
2024-11-26 14:50:03 +0700

    bugfix| spell (#35)
    Co-authored-by: Le Duc Nam <arthit@Joong.local>

--- src/schedules/schedules.service.ts
+++ src/schedules/schedules.service.ts
@@ -95,7 +95,7 @@ export class SchedulesService {
       },
       relations: ['user'],
     });
-    consosle.log('all task for today', response);
+    console.log('all task for today', response);
     response.forEach(async (item: ScheduleEntity) => {
       if (item.repeatType === 'Weekly') {
         const weekDay = weekdays[moment(today).isoWeekday()];
